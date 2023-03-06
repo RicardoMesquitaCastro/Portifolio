faba53c3d4416cc634d2ea2d33c2138759338d03
Ricardo <ricardomesquita12@hotmail.com>
2023-03-06 09:18:34 -0300

    Layout

--- experiencias.mock.ts
+++ experiencias.mock.ts
@@ -4,42 +4,56 @@ export const EXPERIENCIAS: Experiencias[]= [
 
 		{
             id:1,			
-			cargo: "Intelitec Solutions",
-			empresa: "Super Burger",
-			descricao: "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.",
-			data: "December 2011 - March 2013"
+			cargo: "Agrimensor",
+			empresa: "Campo Aberto",
+			descricao: "Responsável por realizar levantamentos topográficos com GPS e HIPER, afim de realizar o georreferenciamento e o Cadastro Ambiental Rural (CAR). As principais ferramentas utilizadas foram, AUTOCAD e GoogleEarth.",
+			data: "Janeiro 2016 - Dezembro 2016"
 			
         },
         {
             id:2,			
-			cargo: "Cargo 1",
-			empresa: "Super Burger1",
-			descricao: "Rodízio de Mini-hambúrger com opção de entrada.1",
-			data: "Original Burger1"
+			cargo: "Técnico em Agropecuária",
+			empresa: "Laticinios JL",
+			descricao: "As funções realizadas eram: Formação e negociação de preço do leite pago ao produtor, mapeamento e roterização de rotas para a coleta de leite e gerenciamento de dados no software PROTHEUS.",
+			data: "Janeiro 2017 - Julho 2018"
 
         },
+		{
+            id:2,			
+			cargo: "Agrimensor",
+			empresa: "AGROCENTER",
+			descricao: "Responsável por realizar levantamentos topográficos com GPS e HIPER, afim de realizar o georreferenciamento e o Cadastro Ambiental Rural (CAR). As principais ferramentas utilizadas foram, AUTOCAD e GoogleEarth. Responsavel também por realizar desmembramentos de terra.",
+			data: "Setembro 2018 - Agosto 2019"
+
+        },{
+			id:3,			
+			cargo: "Repositor de Mercadorias",
+			empresa: "GSA",
+			descricao: "Reposição e controle de estoque exclusivo das marcas Bauduco, Panasonic, Sandela, Haribo.",
+			data: "Novembro 2019 - Agosto 2022"
+	},
         {
             id:3,			
-			"cargo": "restaurante2",
-			"empresa": "Super Burger2",
-			"descricao": "Rodízio de Mini-hambúrger com opção de entrada.2",
-			"data": "Original Burger2"
+			cargo: "Monitor de Lógica em Programação",
+			empresa: "Intstituto Federal Goiano- Campus Urutaí",
+			descricao: "Orientar e tirar dúvidas de alunos do curso de Gestão da Tecnologia da Informação diante da matéria Lógica de Programação utilizando a linguagem de programação C++.",
+			data: "Fevereiro 2020 - Agosto 2020"
 
         },
         {
             id:4,		
-			cargo: "restaurante3",
-			empresa: "Super Burger3",
-			descricao: "Rodízio de Mini-hambúrger com opção de entrada3.",
-			data: "Original Burger3"
+			cargo: "Estagiário em Informática",
+			empresa: "Intstituto Federal Goiano- Campus Urutaí",
+			descricao: "uporte ao usuário, manutenção fisica e lógica de computadores e manutenção de rede.",
+			data: "Agosto 2022 - Dezembro 2022"
 
         },
         {
             id:5,			
-			cargo: "restaurante3l",
-			empresa: "Super Burger3l",
-			descricao: "Rodízio de Mini-hambúrger com opção de entrada3.l",
-			data: "Original Burger3l"
+			cargo: "Desenvolvedor Front-End Angular (Atual)",
+			empresa: "DIX - Soluções Digitais",
+			descricao: "Ferramentas/Habilidades (foco): Trello, lógica de programação, Angular+, Java script, Vue JS, Postman, Usabilidade na Web (UX/UI), CSS, HTML, Mysql, FileZilla, Gitlab e outras ferramentas, para desenvolvimento das atividades; Apoio no levantamento de requisitos e na documentação	técnica dos projetos; Desenvolvimento front conforme escopos, cronogramas e as entregas acordadas; Alinhamento com a área de desenvolvimento back/front/designer para os entendimentos do escopo, cronogramas e as entregas nos prazos; Realizar validações, testes e homologações de produtos desenvolvidos, de forma que as entregas apresentem qualidade, usabilidade e integridade;",
+			data: "Novembro 2022"
 
         }
     ]
